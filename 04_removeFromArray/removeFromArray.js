bd1c8326349a57a03fec46c33134f9cecc15e98b
Raymond Zhong <rzhong669@gmail.com>
2022-04-19 18:11:25 -0500

    Create arg array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(list) {
+  let args = Array.from(arguments);
+  for (i = 0; i < list.length; i++) {
+    let isTarget = false;
+    for (target of args) { // Marks target item for deletion
+      if (list[i] === target) {
+        isTarget = true;
+      }
+    }
+    if (isTarget) {
+      list.splice(i, 1); // Deletes item at current index
+    }
+  }
+  return list;
 };
 
 // Do not edit below this line
