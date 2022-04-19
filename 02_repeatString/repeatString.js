c81da3b6cd9f0f78a6e74da5182fc4d8ea1b1970
Raymond Zhong <rzhong669@gmail.com>
2022-04-18 22:17:28 -0500

    Create repeat string function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, repeats) {
+  let concat = '';
+  if (repeats < 0) {
+    return 'ERROR';
+  }
+  for (let i = 0; i < repeats; i++) {
+    concat += string;
+  }
+  return concat;
 };
 
 // Do not edit below this line
