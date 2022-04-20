5e3b7c479e9a07892a8d0f10d61188d4c80d5934
Raymond Zhong <rzhong669@gmail.com>
2022-04-20 16:29:23 -0500

    Create function to calculate current fibonacci sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+  if (num < 0) {
+    return 'OOPS';
+  }
+  prevNum = 0;
+  currentNum = 1;
+  for(i = 2; i < num; i++) {
+    let add = prevNum + currentNum;
+    prevNum = currentNum;
+    currentNum = add;
+  }
+  return prevNum + currentNum;
 };
 
 // Do not edit below this line
