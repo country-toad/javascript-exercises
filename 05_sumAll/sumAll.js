baf87ffeae847b032efbb98a9434b08c7f6b3f24
Raymond Zhong <rzhong669@gmail.com>
2022-04-19 22:20:24 -0500

    Create check to set end to the larger value

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+  let finalSum = 0;
+  if (start > end) {
+    let swap = 0;
+    swap = start;
+    start = end;
+    end = swap;
+  }
+  for (x = start; x < end + 1; x++) {
+    finalSum += x;
+  }
+  return finalSum;
 };
 
 // Do not edit below this line
