9aef1ba6b5d49acac04d568b80562371032a4fac
Raymond Zhong <rzhong669@gmail.com>
2022-04-20 00:16:43 -0500

    Created function that filters out non-letters, reverses the string, and compares

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (input) {
+  let parsed = input.replace(/[^a-zA-Z]/g, '');
+  parsed = parsed.toLowerCase();
+  list = parsed.split('');
 
+  let revString = '';
+  for (let i = list.length-1; i >= 0; i--) {
+    revString += list[i];
+  }
+  return revString === parsed;
 };
 
 // Do not edit below this line
