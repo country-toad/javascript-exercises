e614f46164fea985f55c718761e1caa39c55a42b
Raymond Zhong <rzhong669@gmail.com>
2022-04-19 22:02:05 -0500

    Create remove items from array function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(list) {
+  let newList = [];
+  let args = [...arguments];
+  let isTarget;
+  for (let x = 0; x < list.length; x++) {
+    isTarget = false;
+    for (y of args) {
+      if (list[x] === y) {
+        isTarget = true;
+      }
+    }
+    if (!isTarget) {
+      newList.push(list[x]);
+    }
+  }
+  return newList;
 };
 
 // Do not edit below this line
