8e317f227d2814b398f824a5a76848f24d72617a
Raymond Zhong <rzhong669@gmail.com>
2022-04-19 23:18:17 -0500

    Round answers to first decimal place

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
 const ftoc = function(temp) {
-  return (temp - 32) * 5 / 9;
+  convert = (temp - 32) * 5 / 9;
+  return Math.round(convert * 10) / 10;
 };
 
 const ctof = function(temp) {
-  return temp * 9 / 5 + 32
+  convert = temp * 9 / 5 + 32;
+  return Math.round(convert * 10) / 10;
 };
 
 // Do not edit below this line
