9a07795dd9487efc421c0acbabd10619015cc6dc
Raymond Zhong <rzhong669@gmail.com>
2022-04-20 19:09:36 -0500

    Create function to find oldest person

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  let oldestAge = 0;
+  let oldestPerson = '';
+  for (person of people) {
+    let age = person.yearOfDeath - person.yearOfBirth;
+    if (age > oldestAge) {
+      oldestPerson = person;
+      oldestAge = age;
+    }
+  }
+  return oldestPerson;
 };
 
 // Do not edit below this line
