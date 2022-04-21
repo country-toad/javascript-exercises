81f684209a12ceb021d43518716e048e5206d93f
Raymond Zhong <rzhong669@gmail.com>
2022-04-20 21:14:06 -0500

    Add functionality to shift individual letters

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,6 +1,33 @@
-const caesar = function() {
+const caesar = function(input, shift) {
+  shift = shift % 26;
+  let newCharCode = input.charCodeAt(0) + shift;
+  return String.fromCharCode(newCharCode);
+
+
+
+
+
+
+
+
+  /*for (char = 0; char < string.length; char++) {
+    let charCode = string.charCodeAt(char);
+    if (charCode >= 65 && charCode <= 90) { // between a-z
+      newChar = charCode + shift;
+       if (newChar > 90) {
+         shiftString += String.fromCharCode(newChar + 64);
+       }
+       if (newChar < 65) {
+         shiftString += String.fromCharCode(newChar - 91;)
+       }
+    } 
+  }*/
+
 
 };
 
+/*
+65 - 90
+97 - 122 */
 // Do not edit below this line
 module.exports = caesar;
